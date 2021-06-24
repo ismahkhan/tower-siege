849dbb0dd93593cf89c49e4999181c2d2a97f067
ismahkhan <82942120+ismahkhan@users.noreply.github.com>
2021-06-24 16:11:19 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,38 @@
+class Launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB=pointB
+        this.launcher = Constraint.create(options);
+        World.add(world, this.launcher);
+    }
+
+    attach(body){
+        this.launcher.bodyA=body;
+    }
+
+    fly(){
+        this.launcher.bodyA=null;
+    }
+
+    display(){
+        if(this.launcher.bodyA){
+        var pointA = this.launcher.bodyA.position;
+        var pointB = this.pointB;
+        
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+}
+}
+
+function keyPressed(){
+	if(keyCode===32){
+	Matter.Body.setPosition(this.polygon,{x:150, y:200})
+	launcher.attach(this.polygon)
+	}
+}
